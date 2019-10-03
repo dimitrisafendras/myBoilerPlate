fe87c3d86885a5d11226cfa993603761b508d064
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-03 11:31:26 +0300

    working react router

--- src/app/styles.js
+++ src/app/styles.js
@@ -3,21 +3,5 @@ import { makeStyles } from '@material-ui/styles';
 export const styles = makeStyles({
   root: {
     textAlign: 'center'
-  },
-  logo: {
-    height: '40vmin'
-  },
-  header: {
-    backgroundColor: '#282c34',
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 'calc(10px + 2vmin)',
-    color: 'white'
-  },
-  link: {
-    color: '#09d3ac'
   }
 });
