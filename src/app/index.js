b54db7bf4200cd2790a5ba20401a2b904aa58f6e
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-04 16:25:14 +0300

    added theme provider with the example

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { AboutUs, ConnectedHome } from '../components/pages';
+import { theme } from '../theme';
 
 export const App = ({ store }) => (
   <Provider store={store}>
-    <Router>
-      <Link to="/home">Home</Link>
-      <Link to="/about">About</Link>
-      <Route path="/home" component={ConnectedHome} />
-      <Route path="/about" component={AboutUs} />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Link to="/home">Home</Link>
+        <Link to="/about">About</Link>
+        <Route path="/home" component={ConnectedHome} />
+        <Route path="/about" component={AboutUs} />
+      </Router>
+    </ThemeProvider>
   </Provider>
 );
 
