9ca79a1ab0c27d266cd181caf7a81f5c97ae746e
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-04 15:46:44 +0300

    fixed router

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { AboutUs, ConnectedHome } from '../components/pages';
 
 export const App = ({ store }) => (
   <Provider store={store}>
     <Router>
-      <Link to="/">Home</Link>
+      <Link to="/home">Home</Link>
       <Link to="/about">About</Link>
-      {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-      <Switch>
-        <Route path="/" component={ConnectedHome} />
-        <Route path="/about" component={AboutUs} />
-      </Switch>
+      <Route path="/home" component={ConnectedHome} />
+      <Route path="/about" component={AboutUs} />
     </Router>
   </Provider>
 );
