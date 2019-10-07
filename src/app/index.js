c80d9028c51bc9a288ffd1755a78926235728f7f
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-07 14:16:23 +0300

    added app layout and refactored routing

--- src/app/index.js
+++ src/app/index.js
@@ -4,8 +4,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
 import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Cell } from 'styled-css-grid';
+import { map } from 'lodash-es';
+import uuid from 'uuid';
 import { AboutUs, ConnectedHome } from '../components/pages';
 import { theme } from '../theme';
+import { AppLayout } from '../components/templates';
+import { Footer, Header } from '../components/organisms';
+
+const routes = {
+  '/': ConnectedHome,
+  '/about': AboutUs,
+};
 
 export const App = ({ store }) => (
   <Provider store={store}>
@@ -14,8 +24,19 @@ export const App = ({ store }) => (
       <Router>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Route exact path="/" component={ConnectedHome} />
-        <Route exact path="/about" component={AboutUs} />
+        <AppLayout>
+          <Cell area="header" className="Header">
+            <Header />
+          </Cell>
+          <Cell area="main" className="Main">
+            {map(routes, (component, route) => (
+              <Route exact path={route} component={component} key={uuid.v1()} />
+            ))}
+          </Cell>
+          <Cell area="footer" className="Footer">
+            <Footer />
+          </Cell>
+        </AppLayout>
       </Router>
     </ThemeProvider>
   </Provider>
