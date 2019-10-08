9be544d60c75dd8dbd8a4d09cde8e624ebc68df0
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-08 14:04:41 +0300

    working stuff

--- src/components/molecules/card/index.js
+++ src/components/molecules/card/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { map } from 'lodash-es';
+import uuid from 'uuid';
 import { styles } from './styles';
+import { H4 } from '../../atoms/h4';
 
-export const Card = ({ city, country, id, name, phone, state, street }) => {
+export const Card = ({ data }) => {
   const { card } = styles();
-  return <div className={card} />;
+  return (
+    <div className={card}>
+      {map(data, data => (
+        <H4 key={uuid.v1()}>{data}</H4>
+      ))}
+    </div>
+  );
 };
--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Button from '@material-ui/core/Button';
+import { map } from 'lodash-es';
+import uuid from 'uuid';
 import { fetchBreweries } from '../../../modules/actions';
 import { styles } from './styles';
+import { Card } from '../../molecules/card';
 
 // eslint-disable-next-line no-shadow,react/prop-types
 const Home = ({ fetchBreweries, breweries }) => {
@@ -13,6 +16,9 @@ const Home = ({ fetchBreweries, breweries }) => {
   return (
     <div className={home}>
       <Button onClick={onclick}>Click</Button>
+      {map(breweries, brewery => (
+        <Card key={uuid.v1()} data={brewery} />
+      ))}
     </div>
   );
 };
