0d94e49e403ec7912b9695390dbefc0ba7fcf548
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-11 12:34:13 +0300

    cleanup

--- src/components/molecules/card/index.jsx
+++ src/components/molecules/card/index.jsx
@@ -6,10 +6,11 @@ import { H4 } from '../../atoms/h4';
 
 export const Card = ({ data }) => {
   const { card } = useStyles();
+  console.log('data', data);
   return (
     <div className={card}>
       {map(data, data => (
-        <H4 key={uuid.v1()}>{data}</H4>
+        <H4 key={uuid.v1()} text={data} />
       ))}
     </div>
   );
