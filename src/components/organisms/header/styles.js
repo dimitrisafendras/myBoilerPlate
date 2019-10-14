7268e2157f5ae4373b0b36e638ca26afe8cff142
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-14 14:20:00 +0300

    working stuff

--- src/components/organisms/header/styles.js
+++ src/components/organisms/header/styles.js
@@ -1,11 +1,12 @@
 import { makeStyles } from '@material-ui/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 
 export const useStyles = makeStyles(theme => {
   const { palette } = theme;
   return {
     header: {
       height: '100%',
-      backgroundColor: palette.primary.dark,
+      backgroundColor: fade(palette.primary.dark, 0.2),
     },
   };
 });
