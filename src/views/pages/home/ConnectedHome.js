374dd7318c4213bbe5f4a985b6ca973b8238db84
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-24 11:14:39 +0300

    renamed Components and added index for each one

---
+++ src/views/pages/home/ConnectedHome.js
@@ -0,0 +1,18 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { deleteBreweries, fetchBreweries } from '../../../modules/actions';
+import { Home } from './Home';
+
+const mapStateToProps = ({ breweriesState }) => ({
+  breweries: breweriesState.breweries,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchBreweries: bindActionCreators(fetchBreweries, dispatch),
+  deleteBreweries: bindActionCreators(deleteBreweries, dispatch),
+});
+
+export const ConnectedHome = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
--- src/views/pages/home/index.js
+++ src/views/pages/home/index.js
@@ -1,18 +1,1 @@
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { deleteBreweries, fetchBreweries } from '../../../modules/actions';
-import { Home } from './home';
-
-const mapStateToProps = ({ breweriesState }) => ({
-  breweries: breweriesState.breweries,
-});
-
-const mapDispatchToProps = dispatch => ({
-  fetchBreweries: bindActionCreators(fetchBreweries, dispatch),
-  deleteBreweries: bindActionCreators(deleteBreweries, dispatch),
-});
-
-export const ConnectedHome = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+export { ConnectedHome } from './ConnectedHome';
