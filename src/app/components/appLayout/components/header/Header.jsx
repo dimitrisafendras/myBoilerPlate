9c2a8abda9a0d4d73bb2fbb7e771a2c6cb10c98f
Dimitris Afendras <dimitrios.afendras@alpha.gr>
2019-10-31 13:34:56 +0200

    refactorings

--- src/app/components/appLayout/components/header/Header.jsx
+++ src/app/components/appLayout/components/header/Header.jsx
@@ -4,6 +4,8 @@ import { Box } from '@material-ui/core';
 import { map } from 'lodash-es';
 import uuid from 'uuid';
 import { useStyles } from './styles';
+
+// TODO: ask if routes should be passed as prop, reminder there is an extra layer 'AppLayout' before header tha doesnt need to know
 import { routes } from '../../../../utils';
 
 export const Header = () => {
@@ -15,9 +17,9 @@ export const Header = () => {
       justifyContent="space-around"
       className={header}
     >
-      {map(routes, (value, key) => (
-        <Link key={uuid.v1()} to={key}>
-          {value.title}
+      {map(routes, (page, url) => (
+        <Link key={uuid.v1()} to={url}>
+          {page.title}
         </Link>
       ))}
     </Box>
--- src/app/utils/routes.js
+++ src/app/utils/routes.js
@@ -6,12 +6,12 @@ export const routes = {
     title: 'Home',
     component: ConnectedHome,
   },
-  '/about': {
-    title: 'About Us',
-    component: AboutUs,
-  },
   '/todo': {
     title: 'To Do List',
     component: ConnectedTodo,
+  },
+  '/about': {
+    title: 'About Us',
+    component: AboutUs,
   },
 };
